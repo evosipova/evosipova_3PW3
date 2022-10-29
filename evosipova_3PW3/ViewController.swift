b7fa4434fcd5812876c5979b4a026ebd3163e094
evosipova <evosipova_3@edu.hse.ru>
2022-10-29 16:30:32 +0300

    Initial Commit

---
+++ evosipova_3PW3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  evosipova_3PW3
+//
+//  Created by Elizabeth on 29.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
